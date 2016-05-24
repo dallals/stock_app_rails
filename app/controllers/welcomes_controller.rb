ff7fe05fd4e07f56117d8c2beedf4d8b0448afe4
Sammy Dallal <sammydallal@gmail.com>
2016-05-24 00:16:50 -0700

    Showing stocks on main page

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -2,7 +2,10 @@ class WelcomesController < ApplicationController
 
 
 	def index
-
+		@user_stocks = current_user.stocks
+  		puts "user stocks"
+  		puts @user_stocks
+  		@user = current_user
 	end
 
 
